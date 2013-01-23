aa57704d8e86235cdc20675bd08421e76537907b
minamo <ph.minamo@cytisan.com>
2013-01-24 03:10:18 +0800

    Include jshint settings. Format index.js.

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-/* node.js library for Plurk API 2.0
- */
-
-module.exports.PlurkClient = require('./lib/PlurkClient');
-
 var fromFile = require('./lib/fromFile');
-module.exports.fromFile = fromFile.fromFile;
-module.exports.fromFileSync = fromFile.fromFileSync;
 
-module.exports.limitedTo = require('./lib/limitedTo');
-
-module.exports.base36 = require('./lib/base36');
-
-module.exports.urlMatch = require('./lib/urlMatch');
+module.exports = {
+    PlurkClient: require('./lib/PlurkClient'),
+    fromFile: fromFile.fromFile,
+    fromFileSync: fromFile.fromFileSync,
+    limitedTo: require('./lib/limitedTo'),
+    base36: require('./lib/base36'),
+    urlMatch: require('./lib/urlMatch')
+};
